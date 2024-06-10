3e765187c9f28cdaacf35c23806d124f193e2e53
chihiro <chihiro.naito8@gmail.com>
2024-06-10 14:50:25 +0900

    correct to extent polygon A*

--- ANALYSIS/Timeseries/03_timeseries_pixels.py
+++ ANALYSIS/Timeseries/03_timeseries_pixels.py
@@ -50,7 +50,7 @@ Malaysia_land_shape = r"F:\MAlaysia\AOI\extent\Malaysia_and_Indonesia_extent_div
 GOSIF_list = glob.glob(GOSIF_dir+"\\*.tif")
 # GPP_list = glob.glob(GPP_dir+"\\*.tif")
 # KBDI_list = glob.glob(KBDI_dir+"\\*.tif")
-VOD_list = glob.glob(VPD_dir+"\\*.tif")
+VPD_list = glob.glob(VPD_dir+"\\*.tif")
 rain_list = glob.glob(rain_dir+"\\*.tif")
 temp_list = glob.glob(temp_dir+"\\*.tif")
 Et_list = glob.glob(Et_dir+"\\*.tif")
@@ -65,16 +65,16 @@ VODDSC2_list = glob.glob(VODDSC2_dir+"\\*.tif")
 
 
 data_kinds_dic = {#"GPP":GPP_list,"KBDI":KBDI_list,
-                  "GOSIF":GOSIF_list,
-                  "rain":rain_list,
-                  "temp":temp_list,
-                  "Et":Et_list,
-                  "Eb":Eb_list,
-                  "VOD":VOD_list,
-                  "SMDSCE":SMDSCE_list,
-                  "VODDSCE":VODDSCE_list,
-                  "SMDSC2":SMDSC2_list,
-                  "VODDSC2":VODDSC2_list
+                   "GOSIF":GOSIF_list,
+                   "rain":rain_list,
+                   "temp":temp_list,
+                    "Et":Et_list,
+                   "Eb":Eb_list,
+                   "VPD":VPD_list,
+                   "SMDSCE":SMDSCE_list,
+                   "VODDSCE":VODDSCE_list,
+                   "SMDSC2":SMDSC2_list,
+                   "VODDSC2":VODDSC2_list
                   }
 
 """
@@ -120,7 +120,7 @@ def get_yyyyddmm(tif_path,datatype):
     if datatype == "KBDI":
         yyyyddmm_str = filename.split("_")[1]
         yyyymmdd_use = datetime.datetime.strptime(yyyyddmm_str, '%Y%m%d')
-    if datatype == "VOD":
+    if datatype == "VPD":
         yyyyddmm_str = filename.split("_")[1]
         yyyymmdd_use = datetime.datetime.strptime(yyyyddmm_str, '%Y%m%d')
     if datatype == "GPP":
@@ -168,38 +168,40 @@ def get_yyyyddmm(tif_path,datatype):
 # 処理
 """
 ## First, obtain GOSIF crop shape
-tiflist = GOSIF_list
-tif = tiflist[-1]
-with rasterio.open(tif) as src:
-    data = src.read()
-    ### ---------------------------------
-    #crop to extent
-    ### ---------------------------------
-    cropped_ras, raster_transform = mask(src, extent_polygon.geometry, crop=True)
-    # meta=src.meta.copy()
-    # meta.update({"transform":raster_transform,"width":shape_use[1],"height":shape_use[0],
-    #               "crs":proj_crs})
-    if len(cropped_ras.shape) ==3:
-        cropped_ras = cropped_ras[0]
-    else:
-        pass
-    sif_shape = cropped_ras.shape
-    
-    # ### check
-    # meta=src.meta.copy()
-    # meta.update({"transform":raster_transform,"width":cropped_ras.shape[1],"height":cropped_ras.shape[0],
-    #               "crs":proj_crs})
-    # extent_dir = os.path.dirname(tif)+"\\extent"
-    # os.makedirs(extent_dir,exist_ok=True)        
-    # path_to_output = extent_dir +f"\\{os.path.basename(tif[:-4])}_extent.tif"
-    # with rasterio.Env(OSR_WKT_FORMAT="WKT2_2018"):
-    #     with rasterio.open(path_to_output,"w",**meta) as dst:
-    #         dst.write(cropped_ras,1)
-    
+def extent_shape(extent_poly):
+    tiflist = GOSIF_list
+    tif = tiflist[-1]
+    with rasterio.open(tif) as src:
+        data = src.read()
+        ### ---------------------------------
+        #crop to extent
+        ### ---------------------------------
+        cropped_ras, raster_transform = mask(src, extent_polygon.geometry, crop=True)
+        # meta=src.meta.copy()
+        # meta.update({"transform":raster_transform,"width":shape_use[1],"height":shape_use[0],
+        #               "crs":proj_crs})
+        if len(cropped_ras.shape) ==3:
+            cropped_ras = cropped_ras[0]
+        else:
+            pass
+        sif_shape = cropped_ras.shape
+        
+        # ### check
+        # meta=src.meta.copy()
+        # meta.update({"transform":raster_transform,"width":cropped_ras.shape[1],"height":cropped_ras.shape[0],
+        #               "crs":proj_crs})
+        # extent_dir = os.path.dirname(tif)+"\\extent"
+        # os.makedirs(extent_dir,exist_ok=True)        
+        # path_to_output = extent_dir +f"\\{os.path.basename(tif[:-4])}_extent.tif"
+        # with rasterio.Env(OSR_WKT_FORMAT="WKT2_2018"):
+        #     with rasterio.open(path_to_output,"w",**meta) as dst:
+        #         dst.write(cropped_ras,1)
+        
+        return sif_shape
     
 
 ## Make arr shape same as gosif's shape
-def set_shape(arr, shape_use = sif_shape):
+def set_shape(arr, shape_use):
     # arr = cropped_ras
     height_diff = arr.shape[0] - shape_use[0]
     width_diff = arr.shape[1] - shape_use[1]
@@ -216,7 +218,7 @@ def set_shape(arr, shape_use = sif_shape):
     # width
     if width_diff ==1:
         arr_cut = np.delete(arr_cut, -1, 1) #drop top rows
-    elif height_diff ==2:
+    elif width_diff ==2:
         arr_cut = np.delete(arr_cut, [0,-1], 1) #drop top and botom rows
     else:
         arr_cut = arr_cut
@@ -226,76 +228,86 @@ def set_shape(arr, shape_use = sif_shape):
 
 ## process for each extent
 for pagename, extent in extent_polygons.items():
+    # pagename,extent = "A1", extent_polygons["A1"]
     
     ## crop other rasters to same as sif.shape 
+    sif_shape = extent_shape(extent)
+    
     for kind, tiflist in data_kinds_dic.items():
         # kind, tiflist = "GOSIF", data_kinds_dic["GOSIF"]
         # kind, tiflist = "GPP", data_kinds_dic["GPP"]
         # kind, tiflist = "KBDI", data_kinds_dic["KBDI"]
         # kind, tiflist = "rain", data_kinds_dic["rain"]
-        # kind, tiflist = "SMASC", data_kinds_dic["SMASC"]
-        # kind, tiflist = "VODASC", data_kinds_dic["VODASC"]
+        # kind, tiflist = "SMDSC2", data_kinds_dic["SMDSC2"]
+        # kind, tiflist = "VODDSC2", data_kinds_dic["VODDSC2"]
         # kind, tiflist = "temp", data_kinds_dic["temp"]
         # kind, tiflist = "Et", data_kinds_dic["Et"]
-        print(kind)
-        date_flatten_dic = {}
-        for tif in tqdm(tiflist):
-            # tif = tiflist[1]
-            filedate = get_yyyyddmm(tif, kind)
-            # Read the raster data
-            src = rasterio.open(tif)
-            data = src.read()
-    
-            ### ---------------------------------
-            #crop to extent
-            ### ---------------------------------
-            cropped_ras, raster_transform = mask(src, extent_polygon.geometry, crop=True)
-            # meta=src.meta.copy()
-            # meta.update({"transform":raster_transform,"width":shape_use[1],"height":shape_use[0],
-            #               "crs":proj_crs})
-            if len(cropped_ras.shape) ==3:
-                cropped_ras = cropped_ras[0]
-            else:
-                pass
-            
-            ### TemperatureとKBDIがたぶんずれててshape違うので臨時で修正
-            remove_arr = set_shape(cropped_ras, shape_use = sif_shape)
-            cropped_ras = remove_arr
-                
-            # if cropped_ras.shape == (72, 204): #事前に確認しないといけない #Malay(72, 204) #GOSIF:(210, 129)
-            # # if kind == "temp" or kind =="KBDI" or kind =="SMDSC" or kind =="VODDSC":
-            #     remove_arr = cropped_ras[:,:-1]
-            #     cropped_ras = remove_arr
-            # else:
-            #     pass
-            
-            # ### check
-            # meta=src.meta.copy()
-            # meta.update({"transform":raster_transform,"width":cropped_ras.shape[1],"height":cropped_ras.shape[0],
-            #               "crs":proj_crs})
-            # extent_dir = os.path.dirname(tif)+"\\extent"
-            # os.makedirs(extent_dir,exist_ok=True)        
-            # path_to_output = extent_dir +f"\\{os.path.basename(tif[:-4])}_extent.tif"
-            # with rasterio.Env(OSR_WKT_FORMAT="WKT2_2018"):
-            #     with rasterio.open(path_to_output,"w",**meta) as dst:
-            #         dst.write(cropped_ras,1)
-            
-            ### ---------------------------------
-            #flatten and interelation
-            ### ---------------------------------
-            cropped_flatten = np.ravel(cropped_ras) #縦になってる(22321,)
-            date_flatten_dic[filedate] = cropped_flatten
-            
-            src.close()
-            
-        ### to dataframe
-        print(cropped_ras.shape)
-        df = pd.DataFrame(date_flatten_dic)
-        # to csv
+        
         final_outdir = out_dir + os.sep + pagename
         os.makedirs(final_outdir, exist_ok=True)
         outfile = final_outdir + f"\\{pagename}_{kind}_pixels_dates.csv"
-        df.to_csv(outfile)
+        
+        if os.path.isfile(outfile):
+            continue
+        else:
+            print(outfile)
+            date_flatten_dic = {}
+            for tif in tqdm(tiflist):
+                # tif = tiflist[1]
+                # tif = [t for t in tiflist if "20120703" in t][0]
+                filedate = get_yyyyddmm(tif, kind)
+                # Read the raster data
+                src = rasterio.open(tif)
+                data = src.read()
+        
+                ### ---------------------------------
+                #crop to extent
+                ### ---------------------------------
+                cropped_ras, raster_transform = mask(src, extent.geometry, crop=True)
+
+                if len(cropped_ras.shape) ==3:
+                    cropped_ras = cropped_ras[0]
+                else:
+                    pass
+                
+                ### TemperatureとKBDIがたぶんずれててshape違うので臨時で修正
+                remove_arr = set_shape(cropped_ras, shape_use = sif_shape)
+                cropped_ras = remove_arr
+                    
+                # if cropped_ras.shape == (72, 204): #事前に確認しないといけない #Malay(72, 204) #GOSIF:(210, 129)
+                # # if kind == "temp" or kind =="KBDI" or kind =="SMDSC" or kind =="VODDSC":
+                #     remove_arr = cropped_ras[:,:-1]
+                #     cropped_ras = remove_arr
+                # else:
+                #     pass
+                
+                # ### check
+                # meta=src.meta.copy()
+                # meta.update({"transform":raster_transform,"width":cropped_ras.shape[1],"height":cropped_ras.shape[0],
+                #               "crs":proj_crs})
+                # extent_dir = os.path.dirname(tif)+"\\extent"
+                # os.makedirs(extent_dir,exist_ok=True)        
+                # path_to_output = extent_dir +f"\\{os.path.basename(tif[:-4])}_extent.tif"
+                # with rasterio.Env(OSR_WKT_FORMAT="WKT2_2018"):
+                #     with rasterio.open(path_to_output,"w",**meta) as dst:
+                #         dst.write(cropped_ras,1)
+                
+                ### ---------------------------------
+                #flatten and interelation
+                ### ---------------------------------
+                cropped_flatten = np.ravel(cropped_ras) #縦になってる(22321,)
+                date_flatten_dic[filedate] = cropped_flatten
+                
+                src.close()
+                
+            ### to dataframe
+            print(cropped_ras.shape)
+            df = pd.DataFrame(date_flatten_dic)
+            # to csv
+            final_outdir = out_dir + os.sep + pagename
+            os.makedirs(final_outdir, exist_ok=True)
+            outfile = final_outdir + f"\\{pagename}_{kind}_pixels_dates.csv"
+            df.to_csv(outfile)
         
        
     
